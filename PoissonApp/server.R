8db4583f5e7b86c577484ddd32afd7ea53001363
Iga Korneta <iga.korneta@gmail.com>
2014-08-11 14:41:25 +0200

    Adding Shiny app for Data Products assignment
    The PoissonApp folder contains the Shiny app for the Data Products
    course assignment

---
+++ PoissonApp/server.R
@@ -0,0 +1,37 @@
+library(ggplot2)
+library(shiny)
+
+shinyServer(
+  function(input, output) {      
+      n <<- reactive ({as.numeric(input$n)})
+      lambda <<- reactive ({as.numeric(input$lambda)})
+      x <- reactive ({data.frame(samples=rpois(n(), lambda()))})
+      
+      output$myPoisPlot <- renderPlot({    
+      p <- ggplot(x(), aes(samples)) + geom_bar(fill="light green") + theme_bw() + xlab("Random samples") + ylab("Count")
+      p
+      #hist(x, xlab='child height', col='lightblue',main='Histogram')
+      #text(63, 150, paste("mu = ", lambda))
+      #text(63, 140, paste("MSE = ", round(lambda, 2)))
+    })
+    
+      sumtable <- reactive({
+        sumtable <- data.frame(row.names=NULL)
+        for (i in levels(as.factor(x()$samples))) {
+          sumtable[i,"Values"] <- i
+          sumtable[i,"Count"] <- print(sum(x()$samples==i))}
+        colnames(sumtable)=c("Values", "Count")
+        rownames(sumtable)<-NULL
+        sumtable })
+    
+       output$myPoisTable <- renderTable({
+           if (input$id2=="TRUE") {sumtable()}
+         }) 
+    
+    
+      output$summary <- renderPrint({
+        if (input$id3=="TRUE") {summary(x())}
+      })
+
+  }
+)
---
+++ PoissonApp/ui.R
@@ -0,0 +1,23 @@
+shinyUI(pageWithSidebar(
+  headerPanel("Poisson distribution"),
+  sidebarPanel(
+    p('This app displays n random samples drawn from a Poisson distribution with parameter lambda.'),
+    p('It was inspired by Project 1 assigned in the Statistical Inference course.'),
+    p('Iga Korneta, on August 11, A.D. 2014'),
+    p('**********'),
+    p(' '),
+    p('Please pick a number of random samples (n) and the lambda parameter:'),
+    sliderInput('n', 'Number of random samples', value = 100, min=1, max=50000, step=1),
+    sliderInput('lambda', 'Lambda', value = 0.5, min=0, max=20, step=0.01),
+    checkboxInput("id2", "Show value counts?", FALSE),
+    checkboxInput("id3", "Show summary?", FALSE)
+  ),
+  mainPanel(
+    h4("Plot"),
+    plotOutput("myPoisPlot"),
+    h4("Counts"),
+    tableOutput("myPoisTable"),
+    h4("Summary"),
+    verbatimTextOutput("summary")
+  )
+))
